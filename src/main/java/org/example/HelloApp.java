5b9fc9205b340f3e0a412fd02119e1f9230c3e0e
Kishore2980 <ka5067@srmist.edu.in>
2026-04-02 13:56:02 +0530

    uc4

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,14 +1,23 @@
 package org.example;// UC 3 - Display "Hello" with Command-Line Argument or Default Message
 
-public class HelloApp{
+// UC4 - Display hello
+
+public class HelloApp {
 
     public static void main(String[] args) {
-        String name = "World";
 
-        if (args.length > 0) {
-            name = args[0]; // Use the provided name
+        String name = "";
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+
+            if (i < args.length - 1) {
+                nameBuilder.append(" ");
+            }
         }
 
-        System.out.println("Hello, " + name + "!");
+        name = nameBuilder.toString();
+
+        System.out.println("Hello " + name + "!");
     }
 }
